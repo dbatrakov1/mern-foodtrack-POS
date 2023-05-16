09fbc1d9544314fbff72e904930ad8183e567afc
Denis <dbatrakov1@gmail.com>
2023-05-16 18:31:39 -0500

    save fillings as array in the localStorage

--- client/src/components/CardFilling.js
+++ client/src/components/CardFilling.js
@@ -4,46 +4,84 @@ import { useState,useEffect } from 'react'
 
 function CardFilling(props) {
   const [pick, setPick] = useState(false)
- 
-  let fillingsLimit = 3
+  const fillingsLimit = 3
   
 
   useEffect(() =>{
     function checkIfCardPicked(){
+      //if no fillings in localStorage => do nothing
       if(props.storedFillings == null){
       }
+      //else if the card's title is found in localStorage => highlight it
       else if(props.storedFillings.includes(props.filling.title)){
-        console.log(`highlight ${props.filling.title}`)
         setPick(true)
       }}
     checkIfCardPicked()
-    console.log('useEffect')
   },[props.filling.title, props.storedFillings]) 
 
 
   function pickFilling(){
+    //if no filling picked yet => first filling will be added
     if(props.storedFillings == null){
-      localStorage.setItem("fillings", props.filling.title)
+      localStorage.setItem("fillings", JSON.stringify([props.filling.title]))
       setPick(true)
-      props.setStoredFillings(props.filling.title)
+      props.setStoredFillings([props.filling.title])
+      // console.log('cond 1')
     }
-    else if(props.storedFillings.split(' ').length < fillingsLimit && !props.storedFillings.includes(props.filling.title)){
-      console.log(`fillings number ${props.storedFillings.split(' ').length}`)
-      let temFillings = props.storedFillings + " " + props.filling.title
-      localStorage.setItem("fillings", temFillings)
+    //else if the fillings limit is not achieved and you try to pick filling that is not picked => new filling will be added
+    else if(props.storedFillings.length < fillingsLimit && !props.storedFillings.includes(props.filling.title)){
+      let temFillings = props.storedFillings.concat(props.filling.title)
+      localStorage.setItem("fillings", JSON.stringify(temFillings))
       setPick(true)
       props.setStoredFillings(temFillings)
-    }else if(props.storedFillings.includes(props.filling.title)){
+      // console.log('cond 2')
+    }
+    //else if the filling is already picked and you try to click it => this feeling will be unpicked(removed)
+    else if(props.storedFillings.includes(props.filling.title)){
       let temFillings = props.storedFillings
-      temFillings = temFillings.replace(props.filling.title, '').trim()
-      localStorage.setItem("fillings", temFillings)
+      let index = temFillings.indexOf(props.filling.title)
+      temFillings.splice(index, 1)
+      localStorage.setItem("fillings", JSON.stringify(temFillings))
       setPick(false)
       props.setStoredFillings(temFillings)
+      // console.log('cond 3')
     }
-    else if(props.storedFillings.split(' ').length >= fillingsLimit && !props.storedFillings.includes(props.filling.title)){
-      
+    //else if the fillings limit is achieved and you try to pick a filling that is not picked => nothing happens
+    else if(props.storedFillings.length >= fillingsLimit && !props.storedFillings.includes(props.filling.title)){
+      // console.log('cond 4')
     }
   }
+
+  // function pickFilling(){
+  //   //if no filling picked yet => first filling will be added
+  //   if(props.storedFillings == null){
+  //     localStorage.setItem("fillings", props.filling.title)
+  //     setPick(true)
+  //     props.setStoredFillings(props.filling.title)
+  //     // console.log('cond 1')
+  //   }
+  //   //else if the fillings limit is not achieved and you try to pick filling that is not picked => new filling will be added
+  //   else if(props.storedFillings.trim().split('   ').length < fillingsLimit && !props.storedFillings.includes(props.filling.title)){
+  //     let temFillings = props.storedFillings + "   " + props.filling.title
+  //     localStorage.setItem("fillings", temFillings)
+  //     setPick(true)
+  //     props.setStoredFillings(temFillings)
+  //     // console.log('cond 2')
+  //   }
+  //   //else if the filling is already picked and you try to click it => this feeling will be unpicked(removed)
+  //   else if(props.storedFillings.includes(props.filling.title)){
+  //     let temFillings = props.storedFillings
+  //     temFillings = temFillings.replace(props.filling.title, '').trim()
+  //     localStorage.setItem("fillings", temFillings)
+  //     setPick(false)
+  //     props.setStoredFillings(temFillings)
+  //     // console.log('cond 3')
+  //   }
+  //   //else if the fillings limit is achieved and you try to pick a filling that is not picked => nothing happens
+  //   else if(props.storedFillings.split(' ').length >= fillingsLimit && !props.storedFillings.includes(props.filling.title)){
+  //     // console.log('cond 4')
+  //   }
+  // }
 
   return (
     <div>
--- client/src/components/screens/OrderScreen.js
+++ client/src/components/screens/OrderScreen.js
@@ -5,7 +5,8 @@ import { useState , useEffect} from "react";
 
 const OrderScreen = () => {
   const [fillings, setFillings] = useState([])
-  const [storedFillings, setStoredFillings] = useState(localStorage.getItem("fillings"))
+  // const [storedFillings, setStoredFillings] = useState(localStorage.getItem("fillings"))
+  const [storedFillings, setStoredFillings] = useState(JSON.parse(localStorage.getItem("fillings")))
 
 useEffect(() =>{
     const fetchFillings = async () => {
@@ -27,9 +28,14 @@ const printFillings = fillings.map((item, index) => {
 
   return (
     <>
+      <div className="orderScreenMain">
         <h1>French Crepes</h1>
-        <h3>Add three topings:</h3>
+        <h3>Choose up to three fillings:</h3>
         {printFillings}
+      </div>
+      <div className="orderScreenFixed">
+        <h2>Add to bag</h2>
+      </div>
         
     </>
   );
