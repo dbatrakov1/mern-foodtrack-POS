cf2a993fa503e878dab743114695ca9177eac02f
Denis <dbatrakov1@gmail.com>
2023-05-22 22:07:26 -0500

    Responsive App bar with Drawer, cart badge

--- client/src/components/screens/OrderScreen.js
+++ client/src/components/screens/OrderScreen.js
@@ -1,102 +1,120 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import FilingCards from "../FillingCards"
+import BadgeCart from "../BadgeCart"
 
-import CardFilling from "../CardFilling";
-import { useState , useEffect} from "react";
-import { BsBag } from "react-icons/bs";
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
 
-const OrderScreen = () => {
-  const [fillings, setFillings] = useState([])
-  const [storedFillings, setStoredFillings] = useState(JSON.parse(localStorage.getItem("fillings")))
-  const [bag, setBag] = useState({})
-  const [crepesInBag, setCrepesInBag] = useState([])
-  // const [bag, setBag] = useState(JSON.parse(localStorage.getItem("bag"))?JSON.parse(localStorage.getItem("bag")):{})
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-useEffect(() =>{
-    const fetchFillings = async () => {
-        const data = await (await fetch('/api/public/getallfillings')).json();
-          setFillings(data);
-      };
-    fetchFillings();
-    let getBagFromLocalSt = JSON.parse(localStorage.getItem("bag"))
-    setBag(getBagFromLocalSt)
-    console.log(bag)
-},[]) 
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
 
-
-//Print filling cards
-const printFillings = fillings.map((item, index) => {
-    return(
-        <CardFilling key={index} filling={item} storedFillings={storedFillings} setStoredFillings={setStoredFillings}/>
-    )
-})
-
-//Bag
-function Bag(crepesFillings){
-  this.crepes = [crepesFillings]
-}
-useEffect(() =>{
-  //if bag has something show how many items it has on bag icon
-  // let newBag = new Bag(storedFillings)
-  localStorage.setItem("bag", JSON.stringify(bag))
-},[bag]) 
-
-const addToBag = () => {
-  console.log('Crepe added!!!')
-  if(bag == null){
-    console.log('Condition 1 bag == null')
-
-    let newBag = new Bag(storedFillings)
-    setBag(newBag)
-    setStoredFillings()
-    
-    localStorage.removeItem("fillings");
-  }
-  else if((bag.hasOwnProperty('crepes'))){
-    console.log('Condition 2 crepes property exist')
-    if(bag.crepes.length == 0){
-      console.log(`Condition 2-1 crepes quatity${bag.crepes.length}`)
-    }
-    else if(bag.crepes.length > 0){
-      console.log(`Condition 2-2 crepes quatity${bag.crepes.length}`)
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
       
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        
+      </List>
+    </Box>
+  );
 
-      setCrepesInBag(current => [...current, storedFillings])
-      console.log(crepesInBag)
-      let newBag = new Bag(crepesInBag)
-      setBag(newBag)
-      console.log(bag)
-      setStoredFillings()
-      
-      localStorage.removeItem("fillings");
-    }
-    // let newBag = new Bag(storedFillings)
-    
-    
-    // setBag(1)
-    // console.log(bag)
-    // localStorage.setItem("bag", JSON.stringify(newBag))
-  }
-
-}
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <>
-      <BsBag className="orderScreenBag" size={30}/>
-      <div className="orderScreenBagText">8</div>
-      <div className="orderScreenMain">
-        <div className="">
-          <h1>French Crepes</h1>
-          <h3>Choose up to three fillings:</h3>
-        </div>
-        
-        {printFillings}
-        
-      </div>
-      <div className="orderScreenFixedBottom" onClick={addToBag}>
-        <h2>Add to bag</h2>
-      </div>
-        
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+            <BadgeCart />
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
+          <FilingCards/>
+        </Typography>
+      </Box>
+    </Box>
   );
+}
+
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
-export default OrderScreen;
+export default DrawerAppBar;
